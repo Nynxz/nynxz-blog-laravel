7f9f012cd07fc00afa3f9ad7a6b4ea6f53c6ea74
Henry Lee <contact@nynxz.com>
2024-10-23 11:44:29 +1000

    fix previous commit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
-    <div  class=" flex flex-col p-2 bg-Surface_2/50 items-center border-Crust border-2 rounded-md w-full h-full shadow-black shadow-2xl group/main hover:border-Mauve backdrop-blur-2xl">
-        <div class=" overflow-y-auto w-1/2 mx-auto overflow-x-auto border-Crust border-2 rounded-md  bg-Base px-14 py-2 h-full ">
+    <div  class=" flex flex-row p-2 bg-Surface_2/50 items-center border-Crust border-2 rounded-md w-full h-full shadow-black shadow-2xl group/main hover:border-Mauve backdrop-blur-2xl">
+        <div class="flex-grow  flex-col overflow-y-auto mx-auto overflow-x-auto border-Crust border-2 rounded-md  bg-Base px-14 py-2 h-full ">
         <span class="font-bold text-3xl underline text-Pink mb-2 justify-between flex flex-row">
             <span>
             {{$post['title']}}
