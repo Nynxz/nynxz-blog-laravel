7fff1a736fcdfc459ae64b3a1676151a703fd18b
Henry Lee <contact@nynxz.com>
2024-10-25 09:31:29 +1000

    add blog-posts to filesystems.php (./blog-posts)

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,6 +37,13 @@ return [
             'throw' => false,
         ],
 
+        'blog-posts' => [
+            'driver' => 'local',
+            'root' => base_path('blog-posts'),
+            'serve' => true,
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
