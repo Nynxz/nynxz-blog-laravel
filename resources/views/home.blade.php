0c1547972987f4f23fce46e9fbda3c7a4ed49fe9
Henry Lee <contact@nynxz.com>
2024-10-25 09:29:56 +1000

    update personal links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,23 +31,30 @@
             <div class="bg-Surface_0 p-2 w-fit rounded-md m-2 group/a">
                 <div class="relative mx-auto my-2 w-32 h-32 border-Crust border-2 rounded-full ">
                     <img src="{{asset('mee.png')}}" alt="icloud me max-w-8 max-h-8"/>
-                    <div class="absolute left-0 top-0 w-full h-full inset-0 rounded-full group-hover/a:ring-Mauve group-hover/a:ring-2 animate-pulse"></div>
+                    <div
+                        class="absolute left-0 top-0 w-full h-full inset-0 rounded-full group-hover/a:ring-Mauve group-hover/a:ring-2 animate-pulse"></div>
                 </div>
                 <span class="font-bold underline text-2xl m-2">
                     Contact Me
                 </span>
                 <ul class="flex flex-col gap-2 m-2">
-                    <li class="flex flex-row group cursor-pointer"> <x-icons.email class="w-8 h-8 "/>
-                        <span class="my-auto px-2 text-lg font-bold"> Email </span>
+                    <li class="flex flex-row group cursor-pointer">
+                        <a href="mailto:contact@nynxz.com" class="flex flex-row">
+                            <x-icons.email class="w-8 h-8 "/>
+                            <span class="my-auto px-2 text-lg font-bold"> Email </span>
+                        </a>
                     </li>
-                    <li class="flex flex-row group cursor-pointer"> <x-icons.github class="w-8 h-8 "/>
+                    <li class="flex flex-row group cursor-pointer">
+                        <a href="https://github.com/nynxz" class="flex flex-row" target="_">
+                        <x-icons.github class="w-8 h-8 "/>
                         <span class="my-auto px-2 text-lg font-bold">Github</span>
+                        </a>
                     </li>
-                    <li class="flex flex-row group cursor-pointer"> <x-icons.linkedin class="w-8 h-8 "/>
+                    <li class="flex flex-row group cursor-pointer">
+                        <a href="https://www.linkedin.com/in/henry-lee-3042b27a/" class="flex flex-row" target="_">
+                        <x-icons.linkedin class="w-8 h-8 "/>
                         <span class="my-auto px-2 text-lg font-bold"> LinkedIn</span>
-                    </li>
-                    <li class="flex flex-row group cursor-pointer"> <x-icons.discord class="w-8 h-8 "/>
-                        <span class="my-auto px-2 text-lg font-bold"> Discord </span>
+                        </a>
                     </li>
                 </ul>
             </div>
