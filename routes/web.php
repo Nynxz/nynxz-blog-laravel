34bddd6a2aed9bb56ab07331b4d9b5b974f45fc3
Henry Lee <contact@nynxz.com>
2024-10-25 09:33:15 +1000

    update routes to use controllers

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,15 @@
 <?php
 
-use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function (Request $request) {
-    return view('home')->with(['warning'=> $request->query('warning')]);
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/post/{id}', function (Request $request, $id) {
+Route::get('/post/{id}', [PostController::class, 'index'])->name('post')->middleware('throttle:30,1');
 
-    $post = Cache::rememberForever('post_' . $id, function () use ($id) {
-        return Post::where('slug', $id)->first();
-    });
-
-    return $post
-        ? view('post')->with('post', $post)
-        : redirect()->route('home');
-
-})->name('post')->middleware('throttle:11110,1');
-
-Route::fallback(function () {
-    return redirect()->route('home');
-});
-
+Route::fallback([HomeController::class, 'redirectToIndex']);
 
 Route::get('/confirmation', function (Request $request) {
     return redirect()->route('home', ['warning' =>  $request->get('warning', '/')]);
